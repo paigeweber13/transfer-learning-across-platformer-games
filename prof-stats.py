ae24017c79fc2bc6fbbd57f796567493abc8a4e2
Riley <rileyw13@protonmail.com>
2019-11-06 10:33:12 -0500

    added comment on how to generate profile data. changed sort to tottime

--- prof-stats.py
+++ prof-stats.py
@@ -1,7 +1,9 @@
 import pstats
 from pstats import SortKey
 
+# to generate profiling data, run 
+# `python -m cProfile -o prof.out neat-supermariobros.py`
 p = pstats.Stats('prof.out')
-p.strip_dirs().sort_stats('cumtime').print_stats()
-p.strip_dirs().sort_stats('time').print_stats()
+p.strip_dirs().sort_stats('tottime').print_stats()
+#p.strip_dirs().sort_stats('percall').print_stats()
 
