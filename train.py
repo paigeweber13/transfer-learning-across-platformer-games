79e965089baaf57ea8a247c98e31dc2f51a60a49
theDweeb <sfurgurs@uncc.edu>
2019-11-06 15:15:52 -0500

    Single process training

---
+++ train.py
@@ -0,0 +1,119 @@
+import argparse 
+import retro
+import numpy as np
+import cv2 
+import neat
+import pickle
+import fileinput
+import sys
+import glob
+import visualize
+
+parser = argparse.ArgumentParser(description='Training')
+# Network/training arguments
+parser.add_argument('-c', '--checkpoint', default=' ', type=str,
+                    help='Path to checkpoint file')
+parser.add_argument('-d', '--downscale', default=8, type=int,
+                    help='How much to reduce input dimensons (X / N)')
+parser.add_argument('-g', '--game', default='SuperMarioBros-Nes', type=str,
+                    help='Name of the game environment')
+parser.add_argument('-r', '--record', default=False, type=bool,
+                    help='Record replays into "./replays"')
+parser.add_argument('-s', '--state', default='Level1-1', type=str,
+                    help='State for the game environment')
+
+args = parser.parse_args()
+
+
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        env = retro.make(game=args.game, state=args.state, record=args.record)
+        ob = env.reset()
+
+        inx = int(ob.shape[0]/args.downscale)
+        iny = int(ob.shape[1]/args.downscale)
+        done = False
+
+        # Model initialization
+        model = neat.nn.RecurrentNetwork.create(genome,config)
+
+        current_max_fitness = 0
+        fitness_current = 0
+        frame = 0
+        counter = 0
+
+        # The downsampled frame that the network sees
+        cv2.namedWindow("network input", cv2.WINDOW_NORMAL)
+        #cv2.resizeWindow("network input", 224,240) # Resize the above window
+        while not done:
+            env.render()
+            frame+=1
+
+            ob = cv2.resize(ob,(inx,iny)) # Ob is the current frame
+            ob = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY) # Colors are not important for learning
+
+            # displays the input to the model (downsampled frames)
+            cv2.imshow('network input', ob)
+            cv2.waitKey(1)
+            ob = np.reshape(ob,(inx,iny))
+
+            oned_image = np.ndarray.flatten(ob)
+            neuralnet_output = model.activate(oned_image) # Give an output for current frame from neural network
+            ob, rew, done, info = env.step(neuralnet_output) # Try given output from network in the game
+
+            fitness_current += rew
+            if fitness_current>current_max_fitness:
+                current_max_fitness = fitness_current
+                counter = 0
+            else:
+                counter+=1
+                # count the frames until it successful
+
+            # Train mario for max 250 frames
+            if done or counter == 250:
+                done = True 
+                print(genome_id,fitness_current)
+
+            genome.fitness = fitness_current
+
+def modifyConfig(file,searchExp,replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
+
+
+
+# Quick work around to be able to change downsample size with command line arguments.
+# xs = 224 // args.downsample
+# ys = 240 // args.downsample
+# zs = 1
+# input_ds = int(xs * ys * zs)
+# modifyConfig('config_feedforward', 'num_inputs = 840', 'num_inputs = %d' % input_ds)
+
+# Load in the changed config file
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                     neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                     './config_feedforward')
+
+# # Change back the config file to original
+# modifyConfig('config_feedforward', 'num_inputs = %s' % input_ds, 'num_inputs = 840')
+
+if args.checkpoint == ' ':
+    p = neat.Population(config)
+else:
+    print('-> Loading %s...' % args.checkpoint)
+    p = neat.Checkpointer.restore_checkpoint("./checkpoints/" + args.checkpoint)
+
+p.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()
+p.add_reporter(stats)
+# Save the process after each x frames
+p.add_reporter(neat.Checkpointer(generation_interval=1000, filename_prefix='checkpoints/SuperMarioBros-neat-'))
+
+winner = p.run(eval_genomes)
+
+visualize.draw_net(config, winner)
+with open('winner.pkl', 'wb') as output:
+    pickle.dump(winner, output, 1)
