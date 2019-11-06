40de038eccfacc5e36d21da172052103784b7cd4
theDweeb <sfurgurs@uncc.edu>
2019-11-05 20:47:37 -0500

    Added argument parser and visual of network input

--- neat-supermariobros.py
+++ neat-supermariobros.py
@@ -1,48 +1,75 @@
-# import os
+import argparse 
 import retro
-import numpy as np # For image-matrix/vector operations
-import cv2 # For image reduction
+import numpy as np
+import cv2 
 import neat
 import pickle
 
-# os.system("python -m retro.import ./roms")
+parser = argparse.ArgumentParser(description='Training')
+# Network/training arguments
+parser.add_argument('-d', '--downsample', default=8, type=int,
+                    help='How much to downsample the resolution for the network input')
+parser.add_argument('-g', '--game', default='SuperMarioBros-Nes', type=str,
+                    help='Game environment')
+parser.add_argument('--record', default=False, type=bool,
+                    help='Record replays into "./replays"')
+parser.add_argument('--render', default=True, type=bool,
+                    help='Choose to render the screen')
+parser.add_argument('--render-freq', dest='render_freq', default=1, type=int,
+                    help='Render the screen every N generations')
+parser.add_argument('-s', '--state', default='Level1-1', type=str,
+                    help='State for the game environment')
+parser.add_argument('--greyscale', default=True, type=bool,
+                    help='RGB to greyscale for the network input')
+
+args = parser.parse_args()
 
 
-# env = retro.make(game='SuperMarioBros-Nes', state='Level1-1',
-#         record='./replays')
-env = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
+env = retro.make(game=args.game, state=args.state, record=args.record)
 
 oned_image = []
+generation = -1
 
 def eval_genomes(genomes, config):
-
+    global generation
+    generation += 1
     for genome_id, genome in genomes:
         
         ob = env.reset() # First image
-        random_action = env.action_space.sample()
+        #random_action = env.action_space.sample()
         inx,iny,inc = env.observation_space.shape # inc = color
+
         # image reduction for faster processing
-        inx = int(inx/8)
-        iny = int(iny/8)
-        # 20 Networks
-        net = neat.nn.RecurrentNetwork.create(genome,config)
+        assert isinstance(args.downsample, int), '--downsample must be an integer value.'
+        inx = int(inx/args.downsample)
+        iny = int(iny/args.downsample)
+
+        # Model initialization
+        model = neat.nn.RecurrentNetwork.create(genome,config)
         current_max_fitness = 0
         fitness_current = 0
         frame = 0
         counter = 0
         
         done = False
-
+        cv2.namedWindow("network input", cv2.WINDOW_NORMAL)
+        #cv2.resizeWindow("network input", 224,240)
         while not done:
-            env.render() # Optional
+            
+            if args.render:
+                if generation % args.render_freq == 0:
+                    env.render() # Optional
             frame+=1
 
             ob = cv2.resize(ob,(inx,iny)) # Ob is the current frame
             ob = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY) # Colors are not important for learning
+            cv2.imshow('network input', ob)
+            cv2.waitKey(1)
             ob = np.reshape(ob,(inx,iny))
-            
+
+
             oned_image = np.ndarray.flatten(ob)
-            neuralnet_output = net.activate(oned_image) # Give an output for current frame from neural network
+            neuralnet_output = model.activate(oned_image) # Give an output for current frame from neural network
             ob, rew, done, info = env.step(neuralnet_output) # Try given output from network in the game
 
             fitness_current += rew
@@ -57,14 +84,13 @@ def eval_genomes(genomes, config):
             if done or counter == 250:
                 done = True 
                 print(genome_id,fitness_current)
-            
-            genome.fitness = fitness_current
 
+            genome.fitness = fitness_current
+            
 config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                      neat.DefaultSpeciesSet, neat.DefaultStagnation,
                      './config_feedforward')
 p = neat.Population(config)
-
 p.add_reporter(neat.StdOutReporter(True))
 stats = neat.StatisticsReporter()
 p.add_reporter(stats)
